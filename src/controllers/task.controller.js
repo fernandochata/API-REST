9a3fbce99e0c309309ac4e51b21fa9b603217018
Fernando Chata <fernando.chata@gmail.com>
2022-06-20 18:10:43 -0400

    update controllers

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -3,15 +3,40 @@ import Task from '../models/task.model.js'
 
 export default {
       async getTasks(req, res) {
-            const tasks = await Task.find()
-            return res.json(tasks)
+            try {
+                  const tasks = await Task.find()
+                  return res.json(tasks)
+            } catch (error) {
+                return res.status(500).json({message: `error ${error.message}`})  
+            }
       },
       
-      getTask(req, res)  { return res.json({'message': 'get task'})},
+      getTask(req, res)  {
+            try {
+                  const task = await Task.findById(req.params.id)
+                  return res.json(task)
+            } catch (error) {
+                  return res.status(500).json({message: `error ${error.message}`})
+            }
+      },
 
-      createTask(req, res) { return res.json({'message': 'create task'})},
+      createTask(req, res) {
+            try {
+                  
+            } catch (error) {
+                  return res.status(500).json({message: `error ${error.message}`})
+            }
+      },
 
       updateTask(req, res) { return res.json({'message': 'update task'})},
 
-      deleteTask(req, res) { return res.json({'message': 'delete task'})}
+      deleteTask(req, res) {
+            try {
+                  const task = Task.findByIdAndDelete(req.params.id)
+                  return res.json(task)
+            } catch (error) {
+                  return res.status(500).json({message: `error ${error.message}`})
+            }
+            
+      }
 }
